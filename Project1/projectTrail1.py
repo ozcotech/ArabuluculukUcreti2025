f1983761aef6135b78a4318b878bb3f8aa23d98c
ozcotech <ozkancomert@hotmail.com>
2025-01-14 23:42:34 +0300

    descriptions added.

--- Project1/projectTrail1.py
+++ Project1/projectTrail1.py
@@ -1,18 +1,18 @@
-def arbUcreti(tutar):
+def arbUcreti(tutar): # Arabuluculuk ücretini hesaplayan fonksiyon
     return round(tutar * 0.06, 2)
 
-while True: 
+while True:  # Kullanıcıdan anlaşma miktarını alıp arabuluculuk ücretini hesaplayan döngü
     tutar = input("Anlaşma miktarını girin: ")
     
-    if not tutar.isdigit() or int(tutar) <= 0:
+    if not tutar.isdigit() or int(tutar) <= 0: # Kullanıcının girdiği değerin pozitif bir tam sayı olup olmadığını kontrol eden if bloğu
         print("Hatalı giriş yaptınız. Lütfen pozitif bir tam sayı girin.")
         continue
 
-    tutar = int(tutar)
+    tutar = int(tutar) #
     
-    if tutar < 100000 and arbUcreti(tutar) < 6000:
+    if tutar < 100000 and arbUcreti(tutar) < 6000: # Anlaşma miktarı 100.000 TL'den az ve arabuluculuk ücreti 6.000 TL'den az ise
         print(f"Anlaşma miktarı: {tutar} TL\nArabuluculuk ücreti: 6.000 TL")
         break
-    else:
+    else: # Anlaşma miktarı 100.000 TL'den fazla veya arabuluculuk ücreti 6.000 TL'den fazla ise
         print(f"Anlaşma miktarı: {tutar} TL\nArabuluculuk ücreti: {arbUcreti(tutar)} TL")
         break
